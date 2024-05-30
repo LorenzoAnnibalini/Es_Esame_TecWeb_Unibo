e813591edd37851799abad3fca2de26f082b11d5
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-05-30 16:04:25 +0200

    .

---
+++ 20220114/jquery/soluzione.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    for (let i = 0; i < 6; i++) {
+        $("table").append("<tr>");
+        for(let j = 0; j < 7; j++){
+            $("table").append("<td>");
+            $("table").append(Math.floor((Math.random()*2)+1));
+            $("table").append("</td>");
+        }
+        $("table").append("</tr>");
+    }
+    $(".copia").hide();
+    console.log("Tabella creata");
+    $("button").click(function(){
+      $(".copia").show();
+    });
+  });
---
+++ 20220613/compito_a/jquery/soluzione.js
@@ -0,0 +1,7 @@
+$(Document).ready(function(){
+    $("form").hide();
+    $("span").hide();
+    $("button:gt(0)").hide();
+
+    let response = fetch("../php/compito_a.php");
+});
