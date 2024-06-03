8bf8b9d9b8f49ed5dbcc64907a45586d567d2db7
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-06-03 15:04:03 +0200

    .

--- 20220114/jquery/soluzione.js
+++ 20220114/jquery/soluzione.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
+  var num;
     for (let i = 0; i < 6; i++) {
         $("table").append("<tr>");
         for(let j = 0; j < 7; j++){
-            $("table").append("<td>");
-            $("table").append(Math.floor((Math.random()*2)+1));
-            $("table").append("</td>");
+          num =Math.floor((Math.random()*2)+1);
+          if(num==1){
+            $("table").append("<td style='background: red'>"+num+"</td>");
+          }else{
+            $("table").append("<td style='background: blue'>"+num+"</td>");
+          }
+
         }
         $("table").append("</tr>");
     }
-    $(".copia").hide();
-    console.log("Tabella creata");
-    $("button").click(function(){
-      $(".copia").show();
-    });
+  $(".copia").hide();
+  console.log("Tabella creata");
+    
+  $("button").click(function(){
+    $(".copia").show();
   });
-  //
+
+  $("td").click(function(){
+    $(this).css("background-color","gray");
+    $(this).text("0");
+  });
+
+});
