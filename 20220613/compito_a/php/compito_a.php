2179d35defa1184f07bafa08e6b7499f3ae8f427
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-06-05 08:34:37 +0200

    .

--- 20220613/compito_a/php/compito_a.php
+++ 20220613/compito_a/php/compito_a.php
@@ -1,17 +1,18 @@
 <?php
+    $conn=new mysqli("localhost","root","","esami");
     $id = $_REQUEST["cont"];
+    $string;
     $statoiniziale = array();
     $source=array("1","2","3","4","5","6","7","8","9");
     $random_keys=array_rand($source,8);
-    for ($i=0; $i<9; $i++) {
-        for ($j=0; $j<9; $j++) {
-            $statoiniziale[$i][$j] = 0;
-        }
+    for ($i=0; $i<(9*9); $i++) {
+            $statoiniziale[$i+$j] = 0;
     }
-    for($i=1; $i<9; $i++) {
-        $statoiniziale[rand(0,8)][rand(0,8)] = $random_keys[$i];
+    for($i=0; $i<8; $i++) {
+        $statoiniziale[rand(0,9*9)] = $source[$random_keys[$i]];
     }
-    $sql="INSERT INTO 'sudoku' ('id', 'statoiniziale') VALUES ('".$id."', '".$statoiniziale."')";
-    $conn->query($sql);
+    $string = implode("",$statoiniziale);
+    $sql="INSERT INTO `sudoku`(`id`, `statoiniziale`) VALUES ('".$id."', '".$string."')";
+    $conn -> query($sql);
     echo json_encode($statoiniziale);
 ?>
