9d239ee4e889f92106196ebae81c4ada7cc561d7
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-06-05 11:28:18 +0200

    .

--- 20220613/compito_a/jquery/soluzione.js
+++ 20220613/compito_a/jquery/soluzione.js
@@ -14,33 +14,19 @@ $(Document).ready(function(){
         const riga = document.getElementById("riga").value;
         const colonna = document.getElementById("colonna").value;
         const valore = document.getElementById("valore").value;
-        let response = fetch("../php/nuovaPartita.php",{method: "GET"});
-        let valoreFinale=String();
-        if(riga > 0 && riga <10 && colonna > 0 && colonna < 10 && valore > 0 && valore < 10){
-            let array= response.json();
-            console.log(array);
-            for(let i=0; i<array.lenght; i++){
-                $("table").pend("<tr>");
-                let cells=array[i];
-                for(let j=0; j<cells.lenght; j++){
-                    if(j==colonna && i==riga){
-                        $("table").pend("<td>"+valore+"</td>");
-                        valoreFinale.push(valore);
-                    }else{
-                        $("table").pend("<td>"+cells[j]+"</td>");
-                        valoreFinale.push(cells[j]);
-                    }
-                }
-                $("table").pend("</tr>");
-            }
-            let response = fetch("../php/controlloRisultato.php?ris="+valoreFinale,{method: "GET"});
-        }else{
-            alert ("Errore");
+        if(riga!=0 && colonna!=0 && valore!=0){
+            const nuovoArray = aggiungi(riga, colonna, valore);
         }
-
+        
     });
 
 });
+
+async function aggiungi(riga, colonna, valore){
+    const response = await fetch("../php/controlloRisultato.php?riga="+riga+"&colonna="+colonna+"&valore="+valore,{method: "GET"});
+    const result = await responde.json();
+    return result;
+}
 
 
 async function getNuovaPartita(){
