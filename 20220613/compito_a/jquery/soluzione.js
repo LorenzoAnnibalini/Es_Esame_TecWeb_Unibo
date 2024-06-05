540dca9ecbe5e5300633818b549ed4d0d4cba6d0
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-06-05 08:31:14 +0200

    .

--- 20220613/compito_a/jquery/soluzione.js
+++ 20220613/compito_a/jquery/soluzione.js
@@ -11,4 +11,14 @@ $("button").click(function(){
     let response = fetch("../php/compito_a.php?cont="+cont,{method: "GET"});
     let array= response.json();
     console.log(array);
+    for(let i=0; i<array.lenght; i++){
+        $("table").pend("<tr>");
+        let cells=array[i];
+        for(let j=0; j<cells.lenght; j++){
+            $("table").pend("<td>"+cells[j]+"</td>");
+        }
+        $("table").pend("</tr>");
+    }
+    $("form").show();
+    document.getElementById("form").reset();
 });
