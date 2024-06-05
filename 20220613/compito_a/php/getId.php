f6602ece418631ba397c1655d8ec1104986a7b14
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-06-05 10:58:36 +0200

    .

--- 20220613/compito_a/php/getId.php
+++ 20220613/compito_a/php/getId.php
@@ -1,11 +1,11 @@
 <?php
-    $id=0;
+    $id=array();
     $conn=new mysqli("localhost","root","","esami");
     $sql="SELECT `id` FROM `sudoku`";
     $result = $conn -> query($sql);
     if ($result -> num_rows > 0) {
-        while($row = $result -> fetch_assoc()) {
-            $id++;
+        while($row=$result -> fetch_assoc()) {
+            $id[]=$row["id"];
         }
     } 
     echo json_encode($id);
