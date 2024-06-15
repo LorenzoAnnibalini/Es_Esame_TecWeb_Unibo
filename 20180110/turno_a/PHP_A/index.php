1e244370a92bed15e58ba8e316c65c9d3cdda2db
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-06-15 12:09:42 +0200

    .

--- 20180110/turno_a/PHP_A/index.php
+++ 20180110/turno_a/PHP_A/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -14,3 +15,24 @@
     </div>
   </body>
 </html>
+<?php
+  $conn= new mysqli("localhost","root","","impiccato");
+  echo("php");
+  if($_SERVER("REQUEST_METHOD")=="POST"){
+    echo("post");
+    if(empty($_POST["lettera"])){
+      echo ("ERRORE");
+    }else{
+      echo("ok2");
+      if($_SESSION["parola"] == NULL){
+        $_SESSION["parola"]= rand(1,5);
+        echo ($_SESSION["parola"]);
+      }
+      $sql="SELECT `parola` FROM `parola` WHERE `parola_id` =".$_SESSION["parola"];
+      $conn->query($sql);
+      echo ($result->fetch_assoc());
+    }
+  }else{
+    echo ("ERRORE");
+  }
+?>
